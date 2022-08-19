9adc41e5dc3395ea1160bf6bb5e8435cfd3a35c1
Daro-Sim <daroandrot@gmail.com>
2022-08-19 17:56:47 +0700

    Bookstore Server is finished

---
+++ src/test/java/tgi/group5/bookstore/BookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package tgi.group5.bookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
